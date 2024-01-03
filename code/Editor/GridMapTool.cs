bfe80107613835bd452f813cdcaf31bd3b50a976
bakscratch <lt_8@hotmail.co.uk>
2024-01-03 18:53:55 +0000

    Box select, Search, Collection and some other stuff I forgot
    https://files.facepunch.com/louie/1b0311b1/2024-01-03%2018-45-15.mp4

--- code/Editor/GridMapTool.cs
+++ code/Editor/GridMapTool.cs
@@ -3,6 +3,8 @@ using RogueFPS;
 using Sandbox;
 using Sandbox.UI;
 using System;
+using static Sandbox.HitboxSet;
+
 namespace Editor;
 
 /// <summary>
@@ -28,6 +30,7 @@ public class GridMapTool : EditorTool
 	}
 	public ModelPrefabSelection CurrentSelection { get; set; } = ModelPrefabSelection.Model;
 
+	public string SearchString { get; set; } = "";
 
 	public ListView modellistView { get; set; } = new();
 	public ListView prefablistView { get; set; } = new();
@@ -45,10 +48,10 @@ public class GridMapTool : EditorTool
 	public PrefabFile SelectedPrefab { get; set; }
 	public string SelectedModel { get; set; }
 
-	public Rotation rotation = Rotation.From( 90, 0, 0 );
-
 	public float floors = 0.0f;
+	public int floorCount = 0;
 
+	public Rotation rotation = Rotation.From( 90, 0, 0 );
 	public enum RotationSnap
 	{
 		None = 0,
@@ -66,13 +69,26 @@ public class GridMapTool : EditorTool
 		Move = 2,
 		Copy = 3,
 	}
-
 	public PaintMode CurrentPaintMode { get; set; } = PaintMode.Place;
 
+	public GameObject CurrentGameObjectCollection;
+	public List<GameObject> GameObjectCollection { get; set; } = new();
+
+	public ComboBox collectionDropDown { get; set; } = new();
+	
 	public override void OnEnabled()
 	{
 		var so = EditorTypeLibrary.GetSerializedObject( this );
 		AllowGameObjectSelection = false;
+
+		foreach (var objectinscene in Scene.GetAllObjects(true))
+		{
+			if ( objectinscene.Tags.Has( "Collection" ) && (objectinscene.Parent is Scene) )
+			{
+				collectionDropDown.AddItem( objectinscene.Name );
+				GameObjectCollection.Add( objectinscene );
+			}
+		}
 
 		{
 			var window = new TileWindow( SceneOverlay, Scene );
@@ -95,6 +111,43 @@ public class GridMapTool : EditorTool
 
 			cs.AddRow( so.GetProperty( "resource" ) );
 
+			var collectionrow = Layout.Row();
+			/*var search = new LineEdit();
+			search.MinimumHeight = Theme.RowHeight;
+			search.PlaceholderText = "Search...";
+			search.TextEdited += ( s ) =>
+			{
+				SearchString = s;
+			};
+			*/
+
+			var search = new LineEdit();
+			search.MinimumHeight = Theme.RowHeight;
+			search.PlaceholderText = "Search...";
+			search.TextEdited += OnSearchTextChanged;
+			
+			var collectionLabel = new Label( "Collection" );
+			var collectionCombo = collectionDropDown;
+			collectionrow.AddSpacingCell( 5 );
+
+			var collectionButton = new Button( "", "add" );
+			collectionButton.ButtonType = "clear";
+			collectionButton.Clicked = () =>
+			{
+				var popup = new NewCollectionObjectWindow();
+				popup.Show();
+				popup.gridmapToolDockWidget = this;
+			};
+			collectionButton.MaximumWidth = 32;
+
+			collectionrow.Add( search );
+			collectionrow.AddSpacingCell( 5 );
+			collectionrow.Add( collectionLabel );
+			collectionrow.AddSpacingCell( 5 );
+			collectionrow.Add( collectionCombo );
+			collectionrow.Add( collectionButton );
+
+
 			var modelprefab = row.Add( new SegmentedControl() );
 			modelprefab.AddOption( "Model", "brush" );
 			modelprefab.AddOption( "Prefab", "delete" );
@@ -102,11 +155,12 @@ public class GridMapTool : EditorTool
 			{
 				CurrentSelection = Enum.Parse<ModelPrefabSelection>( s, true );
 				UpdateListViewVisibility();
+				UpdateListViewItems();
 			};
 
 			prefablistView.ItemSize = 64;
 			prefablistView.ItemAlign = Sandbox.UI.Align.SpaceBetween;
-			prefablistView.OnPaintOverride += () => PaintListBackground( modellistView );
+			prefablistView.OnPaintOverride += () => PaintListBackground( prefablistView );
 			prefablistView.ItemPaint = PaintBrushPrefab;
 			prefablistView.ItemSelected = ( item ) =>
 			{
@@ -128,16 +182,18 @@ public class GridMapTool : EditorTool
 
 			modellistView.OnPaintOverride += () => PaintListBackground( modellistView );
 			modellistView.ItemPaint = PaintBrushItem;
+			row.Add( collectionrow );
 			row.Add( cs );
 			row.Add( modellistView );
 			row.Add( prefablistView );
 
 			window.Layout = row;
-			window.MinimumSize = new Vector2( 300, 500 );
+			window.MinimumSize = new Vector2( 300, 600 );
 
 			AddOverlay( window, TextFlag.RightBottom, 10 );
 		}
 		UpdateListViewVisibility();
+		UpdateListViewItems();
 		{
 			var window = new WidgetWindow( SceneOverlay, "Grid Map Controls" );
 			window.MaximumWidth = 300;
@@ -152,10 +208,15 @@ public class GridMapTool : EditorTool
 
 			var Floorbuttons = Layout.Row();
 			Floorbuttons.Spacing = 8;
-			Floorbuttons.Add( new Label( "Floors:" ) );
+			Floorbuttons.Add( new Label( "Floor:" ) );
 			Floorbuttons.AddStretchCell( 1 );
-			Floorbuttons.Add( new Button( "", "arrow_upward" ) { Clicked = DoFloors( Gizmo.Settings.GridSpacing ) } );
-			Floorbuttons.Add( new Button( "", "arrow_downward" ) { Clicked = DoFloors( -Gizmo.Settings.GridSpacing ) } );
+			var floorLabel = new Label( floorCount.ToString() );
+			Floorbuttons.Add( floorLabel );
+			Floorbuttons.AddStretchCell( 1 );
+			Floorbuttons.Add( new Button( "", "arrow_upward" ) { Clicked = () => { DoFloors( 128 )(); floorLabel.Text = floorCount.ToString(); } } );
+			Floorbuttons.Add( new Button( "", "arrow_downward" ) { Clicked = () => { DoFloors( -128 )(); floorLabel.Text = floorCount.ToString(); } } );
+			cs.AddLayout( Floorbuttons );
+
 
 			cs.AddLayout( Rotbuttons );
 			cs.AddLayout( Floorbuttons );
@@ -167,6 +228,33 @@ public class GridMapTool : EditorTool
 			AddOverlay( window, TextFlag.RightTop, 10 );
 		}
 
+	}
+
+	private void OnSearchTextChanged( string searchText )
+	{
+		SearchString = searchText.ToLower();
+		UpdateListViewItems();
+	}
+
+
+	private void UpdateListViewItems()
+	{
+		if ( CurrentSelection == ModelPrefabSelection.Model )
+		{
+			var filteredModelList = modelList
+				.Where( model => model.name.ToLower().Contains( SearchString ) )
+				.ToList();
+			modellistView.SetItems( filteredModelList.Cast<object>() );
+			modellistView.Update(); // Refresh ListView
+		}
+		else if ( CurrentSelection == ModelPrefabSelection.Prefab )
+		{
+			var filteredPrefabList = prefabList
+				.Where( prefab => prefab.ResourceName.ToLower().Contains( SearchString ) )
+				.ToList();
+			prefablistView.SetItems( filteredPrefabList.Cast<object>() );
+			prefablistView.Update(); // Refresh ListView
+		}
 	}
 
 	private void UpdateListViewVisibility()
@@ -181,8 +269,84 @@ public class GridMapTool : EditorTool
 		base.OnDisabled();
 	}
 
+	private Vector3 startSelectionPoint;
+	private Vector3 endSelectionPoint;
+	private bool isSelecting = false;
+
+	private void FillSelectionWithTiles( Vector3 start, Vector3 end, SceneTraceResult tr )
+	{
+		float gridSpacing = Gizmo.Settings.GridSpacing;
+		Vector3 lowerCorner = new Vector3( Math.Min( start.x, end.x ), Math.Min( start.y, end.y ), start.z ); // Use start.z as it's the ground level
+		Vector3 upperCorner = new Vector3( Math.Max( start.x, end.x ), Math.Max( start.y, end.y ), start.z );
+
+		for ( float x = lowerCorner.x; x <= upperCorner.x; x += gridSpacing )
+		{
+			for ( float y = lowerCorner.y; y <= upperCorner.y; y += gridSpacing )
+			{
+				PlaceTileAtPosition( new Vector3( x, y, lowerCorner.z ), tr );
+			}
+		}
+	}
+
+	private void PlaceTileAtPosition( Vector3 position, SceneTraceResult tr )
+	{
+		if ( SelectedModel != null )
+		{
+			var go = new GameObject( true, "GridTile" );
+			go.Components.Create<ModelRenderer>().Model = Model.Load( SelectedModel );
+			go.Components.Create<ModelCollider>().Model = Model.Load( SelectedModel );
+			go.Transform.Position = position;
+			go.Transform.Rotation = Rotation.LookAt( tr.Normal ) * rotation;
+			go.Tags.Add( "sprinkled" );
+		}
+	}
+
+	//GPT Moment
+	private SceneTraceResult ProjectRayOntoGroundPlane( Vector3 rayOrigin, Vector3 rayDirection, float groundHeight )
+	{
+		Vector3 planeNormal = Vector3.Up;
+		Vector3 planePoint = new Vector3( 0, 0, groundHeight );
+
+		// Correct usage of Vector3.Dot based on your environment's API
+		float denom = Vector3.Dot( planeNormal, rayDirection );
+		if ( Math.Abs( denom ) > 0.0001f ) // Ensure not parallel
+		{
+			float num = Vector3.Dot( planeNormal, planePoint );
+			float distance = (num - Vector3.Dot( planeNormal, rayOrigin )) / denom;
+
+			if ( distance >= 0 )
+			{
+				// Calculate the hit point
+				Vector3 hitPoint = rayOrigin + rayDirection * distance;
+				return new SceneTraceResult { Hit = true, EndPosition = hitPoint };
+			}
+		}
+
+		return new SceneTraceResult { Hit = false };
+	}
+
+	List<GameObject> SelectedGroupObjects { get; set; } = new List<GameObject>();
+
+	private void SelectObjectsInBox( BBox selectionBox )
+	{
+
+		foreach ( var obj in Scene.GetAllObjects( false ) )
+		{
+			if ( selectionBox.Contains( obj.Transform.Position ) && obj.Tags.Has( "sprinkled" ) )
+			{
+				SelectedGroupObjects.Add( obj );
+			}
+		}
+	}
+
+
 	public override void OnUpdate()
 	{
+
+		if ( GameObjectCollection is not null )
+		{
+			CurrentGameObjectCollection = GameObjectCollection.FirstOrDefault( x => x.Name == collectionDropDown.CurrentText );
+		}
 
 		if ( resource != null )
 		{
@@ -210,10 +374,10 @@ public class GridMapTool : EditorTool
 					Log.Info( prefab.ResourceName );
 				}
 			}
+			UpdateListViewItems();
+			//modellistView.SetItems( modelList.Cast<object>() );
 
-			modellistView.SetItems( modelList.Cast<object>() );
-
-			prefablistView.SetItems( prefabList.Cast<object>() );
+			//prefablistView.SetItems( prefabList.Cast<object>() );
 		}
 
 		modellistView.ItemsSelected = SetSelection;
@@ -229,6 +393,88 @@ public class GridMapTool : EditorTool
 
 		if ( resource is null )
 			return;
+
+		if ( Gizmo.IsShiftPressed )
+		{
+			var boxtr = Scene.Trace.Ray(cursorRay,5000)
+						.UsePhysicsWorld( true )
+						.WithoutTags( "sprinkled" )
+						.Run();
+
+			if ( !boxtr.Hit )
+			{
+				Vector3 rayOrigin = cursorRay.Position;
+				Vector3 rayDirection = cursorRay.Forward;
+
+				boxtr = ProjectRayOntoGroundPlane( rayOrigin, rayDirection, 0 );
+			}
+
+			if ( Gizmo.WasLeftMousePressed )
+			{
+				startSelectionPoint = boxtr.EndPosition.WithZ(floors).SnapToGrid( Gizmo.Settings.GridSpacing );
+				isSelecting = true;
+			}
+			else if ( isSelecting )
+			{
+				endSelectionPoint = boxtr.EndPosition.WithZ( floors + 128 ).SnapToGrid( Gizmo.Settings.GridSpacing );
+
+				if ( Gizmo.WasLeftMouseReleased )
+				{
+					var bbox = new BBox( startSelectionPoint, endSelectionPoint );
+					SelectObjectsInBox( bbox );
+					isSelecting = false;
+				}
+			}
+
+			if ( Application.IsKeyDown( KeyCode.F ) )
+			{
+				FillSelectionWithTiles( startSelectionPoint, endSelectionPoint, tr );
+				isSelecting = false;
+			}
+
+			if ( Application.IsKeyDown( KeyCode.X ) )
+			{
+				foreach ( var obj in SelectedGroupObjects )
+				{
+					obj.Destroy();
+				}
+			}
+
+			if( SelectedGroupObjects is not null)
+			{
+				using ( Gizmo.Scope( "Selection" ) )
+				{
+					foreach ( var obj in SelectedGroupObjects )
+					{
+						Gizmo.Draw.Color = Color.Red;
+						Gizmo.Draw.LineBBox( obj.GetBounds() );
+					}
+				}
+			}
+			using ( Gizmo.Scope( "selection_box" ) )
+			{
+				var rect = new BBox( startSelectionPoint, endSelectionPoint );
+				Gizmo.Draw.Color = Color.Blue.WithAlpha( 0.25f );
+				Gizmo.Draw.SolidBox( rect );
+				Gizmo.Draw.Color = Color.Blue;
+				Gizmo.Draw.LineBBox( rect );
+			}
+
+			return;
+		}
+		else if ( isSelecting )
+		{	
+			isSelecting = false;
+		}
+		else
+		{
+			if ( SelectedGroupObjects != null )
+			{
+				SelectedGroupObjects.Clear();
+			}
+			startSelectionPoint = Vector3.Zero;
+			endSelectionPoint = Vector3.Zero;
+		}
 
 		if ( Gizmo.IsCtrlPressed && Gizmo.WasLeftMousePressed )
 		{
@@ -252,11 +498,13 @@ public class GridMapTool : EditorTool
 			}
 		}
 
+
 		if ( !Gizmo.IsCtrlPressed && CurrentSelection == ModelPrefabSelection.Model )
 		{
 			if ( Gizmo.WasLeftMousePressed && CurrentPaintMode == PaintMode.Place )
 			{
 				var go = new GameObject( true, "GridTile" );
+				go.Parent = CurrentGameObjectCollection;
 				go.Components.Create<ModelRenderer>().Model = Model.Load( SelectedModel );
 				go.Components.Create<ModelCollider>().Model = Model.Load( SelectedModel );
 				go.Transform.Position = tr.HitPosition.SnapToGrid( Gizmo.Settings.GridSpacing ).WithZ( floors );
@@ -334,7 +582,6 @@ public class GridMapTool : EditorTool
 				go.Transform.Position = tr.HitPosition.SnapToGrid( Gizmo.Settings.GridSpacing ).WithZ( floors );
 				go.Transform.Rotation = Rotation.LookAt( tr.Normal ) * rotation;
 				go.Tags.Add( "sprinkled" );
-				Log.Info( go.Name );
 			}
 			else if ( Gizmo.WasLeftMousePressed && CurrentPaintMode == PaintMode.Remove )
 			{
@@ -381,6 +628,11 @@ public class GridMapTool : EditorTool
 			{
 				using ( Gizmo.Scope( "preview" ) )
 				{
+					Gizmo.Transform = new Transform( tr.HitPosition.SnapToGrid( Gizmo.Settings.GridSpacing ), Rotation.LookAt( tr.Normal ) * rotation );
+					Gizmo.Draw.Color = Gizmo.Colors.Green.WithAlpha( 0.35f );
+					Gizmo.Draw.SolidBox( previewPrefab.GetBounds() );
+					Gizmo.Draw.LineBBox( previewPrefab.GetBounds() );
+
 					Gizmo.Transform = new Transform( tr.HitPosition.SnapToGrid( Gizmo.Settings.GridSpacing ).WithZ( floors ), Rotation.LookAt( tr.Normal ) * rotation );
 					previewPrefab.Transform.Position = tr.HitPosition.SnapToGrid( Gizmo.Settings.GridSpacing ).WithZ( floors );
 					previewPrefab.Transform.Rotation = Rotation.LookAt( tr.Normal ) * rotation;
@@ -513,6 +765,10 @@ public class GridMapTool : EditorTool
 		return false;
 	}
 
+	/// <summary>
+	/// UI Paint
+	/// </summary>
+	/// <param name="widget"></param>
 	private void PaintBrushItem( VirtualWidget widget )
 	{
 		var brush = (ModelList)widget.Object;
@@ -553,8 +809,6 @@ public class GridMapTool : EditorTool
 		Paint.SetFont( "Poppins", 6, 700 );
 		Paint.DrawText( textRect, brush.name );
 	}
-
-
 	private void PaintBrushPrefab( VirtualWidget widget )
 	{
 		var brush = (PrefabFile)widget.Object;
@@ -628,9 +882,32 @@ public class GridMapTool : EditorTool
 
 	private Action DoFloors( float i )
 	{
-		return () => { floors += i; };
+		return () =>
+		{
+			floors += i;
+			if ( i > 0 )
+			{
+				floorCount++;
+			}
+			else if ( i < 0 )
+			{
+				floorCount--;
+			}
+		};
 	}
 
+	public void CreateCollection( string name )
+	{
+		using var scope = SceneEditorSession.Scope();
+		var go = new GameObject( true, name );
+		go.Transform.Position = Vector3.Zero;
+		go.Tags.Add( "collection" );
+		go.Tags.Add( "sprinkled" );
+
+		GameObjectCollection.Add( go );
+
+		Log.Info( "Created collection" );
+	}
 
 	void ClearAll()
 	{
@@ -656,4 +933,58 @@ file class TileWindow : WidgetWindow
 		base.OnPaint();
 
 	}
+}
+public class NewCollectionObjectWindow : BaseWindow
+{
+
+	Widget container;
+	public GridMapTool gridmapToolDockWidget;
+
+	public NewCollectionObjectWindow()
+	{
+		Size = new Vector2( 350, 150 );
+		MinimumSize = Size;
+		MaximumSize = Size;
+		TranslucentBackground = true;
+		NoSystemBackground = true;
+
+		WindowTitle = "Add New Collection Object";
+		SetWindowIcon( "rocket_launch" );
+
+		Layout = Layout.Column();
+		Layout.Margin = 4;
+		Layout.Spacing = 4;
+
+		container = new Widget( this );
+
+		var properties = new PropertySheet( this );
+
+		var nameLabel = new Label.Subtitle( "Add New Collection Object" );
+		nameLabel.Margin = 16;
+
+		var nameEdit = properties.AddLineEdit( "Collection Object Name", "" );
+		var addbutton = new Button.Primary( "Add Collection", "add_circle" );
+		addbutton.MaximumWidth = 100;
+		addbutton.Clicked = () =>
+		{
+			gridmapToolDockWidget.CreateCollection( nameEdit.Text );
+			/*
+			var gameobject = new GameObject(true, nameEdit.Text );
+			gameobject.Transform.Position = Vector3.Zero;
+			gameobject.Tags.Add( "sprinkled" );
+			gameobject.Tags.Add( "collection" );
+			*/
+			gridmapToolDockWidget.collectionDropDown.AddItem( nameEdit.Text );
+			Close();
+		};
+		Layout.Add( nameLabel );
+		Layout.Add( properties );
+		Layout.Add( addbutton );
+		Layout.Add( container );
+
+		Show();
+	}
+	public string SetButtonIcon = "settings";
+
+	public Button buttonIcon;
 }
