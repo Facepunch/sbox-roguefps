29c587a39ca3c5569cda3bbb9979a7fe778736ec
bakscratch <lt_8@hotmail.co.uk>
2024-03-29 01:18:22 +0000

    Better camera shake

--- code/Camera/CameraShake.cs
+++ code/Camera/CameraShake.cs
@@ -4,28 +4,37 @@ public sealed class CameraShake : Component
 {
 	[Property] public CameraComponent Camera { get; set; }
 
-	bool isShaking = false;
-	[Property] public float intensity { get; set; } = 1.0f;
-
-	TimeSince timeSinceShake = 0;
+	private bool isShaking = false;
+	private TimeSince timeSinceShakeStarted;
+	public float Intensity { get; set; } = 1.0f;
+	public float DecayRate { get; set; } = 6.5f;
+	public float Frequency { get; set; } = 10.0f;
 
 	protected override void OnUpdate()
 	{
 		if ( !isShaking ) return;
 
-		if(timeSinceShake > 0.15f)
+		var elapsed = timeSinceShakeStarted;
+		if ( elapsed > 1f )
 		{
 			isShaking = false;
 			return;
 		}
 
-		Camera.Transform.Position = Camera.Transform.Position + new Vector3( Random.Shared.Float( -intensity, intensity ), Random.Shared.Float( -intensity, intensity ), Random.Shared.Float( -intensity, intensity ) );
-		Log.Info( "Shaking" );
+		var currentIntensity = Intensity * MathF.Exp( -DecayRate * elapsed );
+
+		var shakePattern = MathF.Sin( Frequency * elapsed );
+
+		Camera.Transform.Position += new Vector3(
+			shakePattern * Random.Shared.Float( -currentIntensity, currentIntensity ),
+			shakePattern * Random.Shared.Float( -currentIntensity, currentIntensity ),
+			Random.Shared.Float( -currentIntensity, currentIntensity )
+		);
 	}
 
 	public void Shake()
 	{
 		isShaking = true;
-		timeSinceShake = 0;
+		timeSinceShakeStarted = 0;
 	}
 }
