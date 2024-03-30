54085225bfd1239e0b1703eb6e4cadd36d39dca2
bakscratch <lt_8@hotmail.co.uk>
2024-03-30 00:48:26 +0000

    Navmesh based chest spawner

---
+++ code/Items/LootBoxSpawner.cs
@@ -0,0 +1,58 @@
+using Sandbox;
+using Sandbox.Navigation;
+[EditorHandle( "materials/editor/lootspawn.png" )]
+public sealed class LootBoxSpawner : Component
+{
+	[Property] public GameObject LootBoxPrefab { get; set; }
+	[Property] public int SpawnAmount { get; set; } = 10;
+
+	[Property] int currentSpawned = 0;
+
+	protected override void DrawGizmos()
+	{
+		base.DrawGizmos();
+/*
+		for ( int i = 0; i < SpawnAmount; i++ )
+		{
+			var pos = Scene.NavMesh.GetClosestPoint( Vector3.Random * 10000, 800000 );
+			if (pos.HasValue)
+			{
+				Gizmo.Draw.SolidSphere( pos.Value, 10f);
+			}
+		}
+*/
+	}
+
+	protected override void OnStart()
+	{
+		base.OnStart();
+
+		_=SpawnBoxes();
+	}
+
+	protected override void OnUpdate()
+	{
+		if( currentSpawned == SpawnAmount ) return;
+
+
+	}
+
+	async Task SpawnBoxes()
+	{
+		for ( int i = 0; i < SpawnAmount; i++ )
+		{
+			await Task.Delay( 100 );
+			if ( LootBoxPrefab is null ) return;
+			var lootBox = LootBoxPrefab.Clone();
+			var pos = Scene.NavMesh.GetRandomPoint( Transform.Position, 2000 );
+
+			if ( pos.HasValue )
+			{
+				lootBox.Transform.Position = pos.Value;
+				lootBox.Transform.Rotation = Rotation.FromYaw(Random.Shared.Float( -360, 360 ));
+				lootBox.Transform.Rotation = Rotation.FromPitch(Random.Shared.Float( -10, 10 ));
+				lootBox.Transform.Rotation = Rotation.FromRoll(Random.Shared.Float( -10, 10 ));
+			}
+		}
+	}
+}
