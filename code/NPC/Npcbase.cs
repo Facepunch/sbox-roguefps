e5bb00f295c2e37190e27345db409b7d0137c767
bakscratch <lt_8@hotmail.co.uk>
2024-04-03 06:02:51 +0100

    Quick dmg numbers

--- code/NPC/Npcbase.cs
+++ code/NPC/Npcbase.cs
@@ -3,7 +3,7 @@ using System.Numerics;
 
 public sealed class Npcbase : Component, Component.ITriggerListener
 {
-	GameObject Target { get; set; }
+	public GameObject Target { get; set; }
 	[Property] public NavMeshAgent Agent { get; set; }
 
 	[Property] public PlayerStats Stats { get; set; }
@@ -41,6 +41,13 @@ public sealed class Npcbase : Component, Component.ITriggerListener
 
 	public void OnDamage(float damage)
 	{
+		var txt = new TextObject(Scene.SceneWorld);
+		txt.Transform = Transform.World.WithScale(Transform.Scale * 0.2f * Target.Transform.Position.Distance( Transform.Position ) / 200f);
+		txt.Position = Transform.Position + new Vector3( Random.Shared.Float( -20f, 20f ), Random.Shared.Float( -20f, 20f ), Random.Shared.Float(10,50f));
+		txt.Rotation = Rotation.LookAt( Target.Transform.Position - Transform.Position ).Angles().WithPitch(0);
+		txt.Text = damage.ToString();
+		txt.ColorTint = Color.Random;
+
 		Health -= damage;
 		if ( Health <= 0 )
 		{
@@ -62,5 +69,37 @@ public sealed class Npcbase : Component, Component.ITriggerListener
 		}
 
 		Log.Info( "Health: " + Health );
+	}
+}
+
+file class TextObject : SceneCustomObject
+{
+	public float FontSize { get; set; } = 64;
+	public string FontFamily { get; set; } = "Quantico";
+	public string Text { get; set; } = "ELLO!";
+	public TextObject(SceneWorld world ) : base(world)
+	{
+		RenderLayer = SceneRenderLayer.Default;
+	}
+	public override void RenderSceneObject()
+	{
+		if ( string.IsNullOrWhiteSpace( Text ) )
+			return;
+
+		var textFlags = TextFlag.DontClip | TextFlag.Center;
+
+		Graphics.Attributes.SetCombo( "D_WORLDPANEL", 1 );
+
+		// Set a dummy WorldMat matrix so that ScenePanelObject doesn't break the transforms.
+		Matrix mat = Matrix.CreateRotation( Rotation.From( 0, 90, 90 ) );
+		Graphics.Attributes.Set( "WorldMat", mat );
+
+		Graphics.DrawText( new Rect( 0 ), Text, ColorTint, FontFamily, FontSize, 800.0f, textFlags );
+		//Make the text smaller over time
+		Transform = Transform.WithScale(MathF.Sign( Transform.Scale.x ) * (Transform.Scale.x - RealTime.Delta * 0.75f));
+		if( Transform.Scale.x <= 0 )
+		{
+			Delete();
+		}
 	}
 }
