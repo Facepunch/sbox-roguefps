55301d69474a4f2940a084df4281ad1babd3434f
bakscratch <lt_8@hotmail.co.uk>
2024-04-05 00:26:27 +0100

    Clean up

--- code/Items/Interactable.cs
+++ code/Items/Interactable.cs
@@ -2,18 +2,11 @@ using Sandbox;
 
 public class Interactable : Component
 {
-
 	[Property] public virtual string Name { get; set; } = "Interactable";
 	[Property] public bool HasPrice { get; set; } = false;
 	[Property] public int Cost { get; set; } = 100;
 	[Property] public bool IsOpen { get; set; } = false;
-
 	public GameObject Player { get; set; }
-
-	protected override void OnUpdate()
-	{
-
-	}
 
 	public virtual void OnInteract( GameObject player)
 	{
--- code/Items/ItemsAndContent.cs
+++ code/Items/ItemsAndContent.cs
@@ -1,14 +1,11 @@
 ﻿public class ItemsAndContent
 {
-
 	//Lets make a list of all the items in the game
 	public static List<ItemDef> Items = new List<ItemDef>
 	{
-	//	new ItemDefTest(),
 		new AddJump(),
 		new LaunchJump(),
 		new FireHit()
-	//	new ItemDefTestThree()
 	};
 
 }
