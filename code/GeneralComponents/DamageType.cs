f42a5448f6b18c0e85fdde97a3e43d24f4de6055
bakscratch <lt_8@hotmail.co.uk>
2024-04-05 01:58:07 +0100

    Damage types + Get damagetype icon

---
+++ code/GeneralComponents/DamageType.cs
@@ -0,0 +1,23 @@
+﻿public enum DamageTypes
+{
+	None,
+	Fire,
+	Ice,
+	Poison,
+	Electric
+}
+//Get damage type icon from damage type
+public class DamageType
+{
+	public static string GetDamageTypeIcon(DamageTypes dmgType)
+	{
+		return dmgType switch
+		{
+			DamageTypes.Fire => "ui/test/damagetypes/fire_damage_type.png",
+			DamageTypes.Ice => "ui/test/ability/ab2.png",
+			DamageTypes.Poison => "ui/test/ability/ab3.png",
+			DamageTypes.Electric => "ui/test/damagetypes/electric_damage_type.png",
+			_ => "",
+		};
+	}
+}
