aa4cb29f7ec0a946715475d10ab7cdb902b8061c
Matt Stevens <matt@mattstevens.co.uk>
2024-04-05 03:22:18 +0100

    Screen space damage numbers

--- code/NPC/Npcbase.cs
+++ code/NPC/Npcbase.cs
@@ -56,12 +56,8 @@ public sealed class Npcbase : Component, Component.ITriggerListener
 
 	public void OnDamage(float damage, DamageTypes dmgType)
 	{
-		var txt = new TextObject(Scene.SceneWorld);
-		txt.Transform = Transform.World.WithScale(Transform.Scale * 0.2f * Target.Transform.Position.Distance( Transform.Position ) / 200f);
-		txt.Position = Transform.Position + new Vector3( Random.Shared.Float( -20f, 20f ), Random.Shared.Float( -20f, 20f ), Random.Shared.Float(10,50f));
-		txt.Rotation = Rotation.LookAt( Target.Transform.Position - Transform.Position ).Angles().WithPitch(0);
-		txt.Text = damage.ToString();
-		txt.ColorTint = Color.Random;
+		DamageNumbers.Add( (int)damage, Transform.Position + new Vector3( Random.Shared.Float( -20f, 20f ), Random.Shared.Float( -20f, 20f ), Random.Shared.Float( 10, 50f ) ) );
+
 		timeSinceLastDamaged = 0;
 		DamageType = dmgType;
 		Health -= damage;
@@ -85,37 +81,5 @@ public sealed class Npcbase : Component, Component.ITriggerListener
 		}
 
 		//Log.Info( "Health: " + Health );
-	}
-}
-
-file class TextObject : SceneCustomObject
-{
-	public float FontSize { get; set; } = 64;
-	public string FontFamily { get; set; } = "Quantico";
-	public string Text { get; set; } = "ELLO!";
-	public TextObject(SceneWorld world ) : base(world)
-	{
-		RenderLayer = SceneRenderLayer.Default;
-	}
-	public override void RenderSceneObject()
-	{
-		if ( string.IsNullOrWhiteSpace( Text ) )
-			return;
-
-		var textFlags = TextFlag.DontClip | TextFlag.Center;
-
-		Graphics.Attributes.SetCombo( "D_WORLDPANEL", 1 );
-
-		// Set a dummy WorldMat matrix so that ScenePanelObject doesn't break the transforms.
-		Matrix mat = Matrix.CreateRotation( Rotation.From( 0, 90, 90 ) );
-		Graphics.Attributes.Set( "WorldMat", mat );
-
-		Graphics.DrawText( new Rect( 0 ), Text, ColorTint, FontFamily, FontSize, 800.0f, textFlags );
-		//Make the text smaller over time
-		Transform = Transform.WithScale(MathF.Sign( Transform.Scale.x ) * (Transform.Scale.x - RealTime.Delta * 0.75f));
-		if( Transform.Scale.x <= 0 )
-		{
-			Delete();
-		}
 	}
 }
---
+++ code/Player/DamageNumbers.cs
@@ -0,0 +1,70 @@
+﻿using Sandbox;
+using Sandbox.Utility;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace RogueFPS;
+
+public partial class DamageNumbers : Component
+{
+	[RequireComponent]
+	public CameraComponent Camera { get; set; }
+
+	IDisposable renderHook;
+
+	protected override void OnEnabled()
+	{
+		renderHook?.Dispose();
+
+		renderHook = Camera.AddHookAfterUI( "DamageNumbers", 700, RenderEffect );
+	}
+
+	protected override void OnDisabled()
+	{
+		renderHook?.Dispose();
+		renderHook = null;
+	}
+
+	struct DamageNumber
+	{
+		public int Amount;
+		public Vector3 WorldPosition;
+		public RealTimeSince TimeSince;
+	}
+
+	static List<DamageNumber> Numbers = new();
+
+	public static void Add( int amount, Vector3 position )
+	{
+		Numbers.Add( new DamageNumber() { Amount = amount, WorldPosition = position, TimeSince = 0 } );
+	}
+
+	protected override void OnUpdate()
+	{
+		for ( int i = Numbers.Count - 1; i >= 0; i-- )
+		{
+			if ( Numbers[i].TimeSince >= TimeToLive )
+			{
+				Numbers.RemoveAt( i );
+			}
+		}
+	}
+
+	public float TimeToLive => 2.0f;
+	public float RiseAmount => 64.0f;
+
+	public void RenderEffect( SceneCamera camera )
+	{
+		foreach ( var number in Numbers )
+		{
+			var screenPos = camera.ToScreen( number.WorldPosition );
+
+			// Piss around with different Easing methods
+			var ease = Easing.ExpoOut( number.TimeSince / TimeToLive );
+			screenPos -= Vector2.Up * ease * 64;
+
+			var color = Color.White.WithAlpha( 1.0f - Easing.EaseOut( number.TimeSince / TimeToLive ) );
+
+			Graphics.DrawText( screenPos, number.Amount.ToString(), color, "Quantico", 32, 800 );
+		}
+	}
+}
