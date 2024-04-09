a2a8d0803706360b04edb6d2e397248b23e33b46
bakscratch <lt_8@hotmail.co.uk>
2024-04-09 15:45:50 +0100

    Spawn equipment chests

--- code/Items/LootBoxSpawner.cs
+++ code/Items/LootBoxSpawner.cs
@@ -7,6 +7,7 @@ public sealed class LootBoxSpawner : Component
 	[Property] public GameObject MultiShopPrefab { get; set; }
 	[Property] public GameObject ItemScrapper { get; set; }
 	[Property] public GameObject ItemPrinter { get; set; }
+	[Property] public GameObject EquipmentChest { get; set; }
 	[Property] public int SpawnAmount { get; set; } = 10;
 
 	[Property] int currentSpawned = 0;
@@ -94,7 +95,6 @@ public sealed class LootBoxSpawner : Component
 				//multiShop.BreakFromPrefab();
 			}
 		}
-
 		for ( int i = 0; i < SpawnAmount / 3; i++ )
 		{
 			await Task.Delay( 100 );
@@ -110,6 +110,23 @@ public sealed class LootBoxSpawner : Component
 				itemPrinter.Transform.Rotation = Rotation.FromRoll(Random.Shared.Float( -10, 10 ));
 				itemPrinter.Transform.Rotation = Rotation.FromYaw( Random.Shared.Float( -360, 360 ) );
 				//itemPrinter.BreakFromPrefab();
+			}
+		}
+		for ( int i = 0; i < SpawnAmount / 4; i++ )
+		{
+			await Task.Delay( 100 );
+			if ( EquipmentChest is null ) return;
+			var equipmentChest = EquipmentChest.Clone();
+			var pos = Scene.NavMesh.GetRandomPoint( Transform.Position, 2000 );
+
+			if ( pos.HasValue )
+			{
+				equipmentChest.Transform.Position = pos.Value;
+
+				equipmentChest.Transform.Rotation = Rotation.FromPitch(Random.Shared.Float( -10, 10 ));
+				equipmentChest.Transform.Rotation = Rotation.FromRoll(Random.Shared.Float( -10, 10 ));
+				equipmentChest.Transform.Rotation = Rotation.FromYaw( Random.Shared.Float( -360, 360 ) );
+				//equipmentChest.BreakFromPrefab();
 			}
 		}
 	}
