bcc85dd4cbf6ad03285c83a9cbc38a426c256af5
bakscratch <lt_8@hotmail.co.uk>
2024-04-16 16:30:24 +0100

    Wip Menu

---
+++ code/UI/MainMenu/PageCameraSetup.cs
@@ -0,0 +1,8 @@
+﻿public struct PageCameraSetup
+{
+	[KeyProperty] public string Url { get; set; }
+	[KeyProperty] public GameObject CameraObject { get; set; }
+
+	public float PositionLerpSpeed { get; set; }
+	public float RotationLerpSpeed { get; set; }
+}
