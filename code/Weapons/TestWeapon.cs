cc60f670baa9eef47af01264f8caf24bbf191c5c
Tony Ferguson <devultj@gmail.com>
2024-04-16 13:47:52 +0100

    Weapon items have an option for originating from its GameObject instead of the Camera

--- code/Weapons/TestWeapon.cs
+++ code/Weapons/TestWeapon.cs
@@ -8,6 +8,18 @@ public class TestWeapon : BaseWeaponItem
 	public override bool RandomSpread { get; set; } = true;
 	public override float Spread { get; set; } = 15.0f;
 
+	/// <summary>
+	/// The object that we're tracing from.
+	/// </summary>
+	/// <returns></returns>
+	public GameObject GetTraceObject()
+	{
+		if ( UseGameObjectForTrace ) return GameObject;
+		return Scene.Camera.GameObject;
+	}
+
+	[Property] public bool UseGameObjectForTrace { get; set; } = false;
+
 	protected override void OnUpdate()
 	{
 		base.OnUpdate();
@@ -25,14 +37,16 @@ public class TestWeapon : BaseWeaponItem
 		base.OnPrimaryFire();
 
 		Sound.Play( "ui.popup.message.close", GameObject.Parent.Transform.Position );
-		
-		var tr = TraceBullet( Scene.Camera.Transform.Position, Scene.Camera.Transform.Position + Scene.Camera.Transform.Rotation.Forward * 1000f );
+
+		var cameraObject = GetTraceObject();
+
+		var tr = TraceBullet( cameraObject.Transform.Position, cameraObject.Transform.Position + cameraObject.Transform.Rotation.Forward * 1000f );
 
 		ViewModel?.Set("b_attack", true);
 
 		var tracer = BulletTrace.Clone();
 		var tracerParticle = tracer.Components.Get<TracerBulletParticle>();
-		tracerParticle.Start.Transform.Position = tr.StartPosition + Scene.Camera.Transform.Rotation.Forward * 10f;
+		tracerParticle.Start.Transform.Position = tr.StartPosition + cameraObject.Transform.Rotation.Forward * 10f;
 		tracerParticle.End.Transform.Position = tr.EndPosition;
 
 		var inventory = Stats.Inventory;
