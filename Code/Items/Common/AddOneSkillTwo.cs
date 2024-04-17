2ad119f90029c3426e1d29e62e2fc6834637613e
bakscratch <lt_8@hotmail.co.uk>
2024-04-17 14:21:40 +0100

    pretty temp icon

--- Code/Items/Common/AddOneSkillTwo.cs
+++ Code/Items/Common/AddOneSkillTwo.cs
@@ -2,11 +2,10 @@
 {
 	public override string Name => "Add One Skill Two";
 	public override string Description => "Add +1 to your secoundary skill";
-	public override string Icon => "ui/test/items/scrap.png";
+	public override string Icon => "ui/test/items/extendedmags.icon.png";
 	public override ItemTier ItemTier => ItemTier.Common;
 	public override string ItemColor => ColorSelection.GetRarityColor( ItemTier.Common );
-	public override Model Model => Model.Load( "models/citizen_props/trashbag02.vmdl_c" );
-	public override int StatUpgradeAmount => 25;
+	public override Model Model => Model.Load( "models/citizen_props/coffeemug01.vmdl" );
 
 	public override void ApplyUpgrade()
 	{
